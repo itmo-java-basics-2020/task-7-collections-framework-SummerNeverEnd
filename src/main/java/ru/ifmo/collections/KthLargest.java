3fe68aa782ee3c1d4dda9ae89a9dbd6eb9456939
Vladimir Nesterov <vova.nest.2000@yandex.ru>
2020-06-10 14:35:38 +0300

    add implementation of FU, KL and LC

--- src/main/java/ru/ifmo/collections/KthLargest.java
+++ src/main/java/ru/ifmo/collections/KthLargest.java
@@ -1,18 +1,33 @@
 package ru.ifmo.collections;
 
+import java.util.PriorityQueue;
+
 /**
  * Design a class to find the kth largest element in a stream. k is from 1 to numbers.length.
  * Note that it is the kth largest element in the sorted order, not the kth distinct element.
- *
+ * <p>
  * Constructor accepts an integer k and an integer array numbers, which contains initial elements from the stream.
  * For each call to the method KthLargest.add(), return the element representing the kth largest element in the stream.
  */
 public class KthLargest {
+    private final PriorityQueue<Integer> queue;
+    private final int k;
+
     public KthLargest(int k, int[] numbers) {
-        // TODO implement
+        this.k = k;
+        queue = new PriorityQueue<>();
+        for (int number : numbers) {
+            this.add(number);
+        }
     }
 
     public int add(int val) {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (queue.size() < k) {
+            queue.add(val);
+        } else if (queue.peek() < val) {
+            queue.poll();
+            queue.add(val);
+        }
+        return queue.peek();
     }
 }
--- src/main/java/ru/ifmo/collections/LruCache.java
+++ src/main/java/ru/ifmo/collections/LruCache.java
@@ -1,29 +1,39 @@
 package ru.ifmo.collections;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Represents LRU cache with fixed maximum capacity.
- *
+ * <p>
  * get() should return null if there is no value for a given key.
  * elements() should return number of elements in cache.
- *
+ * <p>
  * This class should not have any other public methods.
- *
+ * <p>
  * Implementing this cache in (almost) the same manner as it was implemented during the lecture will result in extra points.
  */
 public class LruCache<K, V> {
+    private final Map<K, V> lruCache;
+
     public LruCache(int capacity) {
-        // TODO implement
+        lruCache = new LinkedHashMap<K, V>(capacity, 1f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+                return size() > capacity;
+            }
+        };
     }
 
     public V get(K key) {
-        throw new UnsupportedOperationException(); // TODO implement
+        return lruCache.get(key);
     }
 
     public void put(K key, V value) {
-        // TODO implement
+        lruCache.put(key, value);
     }
 
     public int elements() {
-        throw new UnsupportedOperationException(); // TODO implement
+        return lruCache.size();
     }
 }
